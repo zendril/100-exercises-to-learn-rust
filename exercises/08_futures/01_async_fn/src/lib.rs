c8bf02e14ec771c1300c9b7296d3b027cde9a04c
zendril <kenneth.s.brooks@gmail.com>
2025-06-20 13:15:45 -0400

    updates before I wipe dir

--- exercises/08_futures/01_async_fn/src/lib.rs
+++ exercises/08_futures/01_async_fn/src/lib.rs
@@ -1,4 +1,6 @@
-use tokio::net::TcpListener;
+use anyhow::{anyhow, Error};
+use tokio::io::{AsyncReadExt, AsyncWriteExt, copy};
+use tokio::net::{TcpListener, TcpStream};
 
 // TODO: write an echo server that accepts incoming TCP connections and
 //  echoes the received data back to the client.
@@ -11,7 +13,11 @@ use tokio::net::TcpListener;
 // - `tokio::net::TcpStream::split` to obtain a reader and a writer from the socket
 // - `tokio::io::copy` to copy data from the reader to the writer
 pub async fn echo(listener: TcpListener) -> Result<(), anyhow::Error> {
-    todo!()
+    loop {
+        let (mut socket, _) = listener.accept().await?;
+        let (mut reader, mut writer) = socket.split();
+        copy(&mut reader, &mut writer).await?;
+    }
 }
 
 #[cfg(test)]
--- exercises/08_futures/02_spawn/src/lib.rs
+++ exercises/08_futures/02_spawn/src/lib.rs
@@ -1,10 +1,27 @@
+use tokio::io::copy;
 use tokio::net::TcpListener;
 
 // TODO: write an echo server that accepts TCP connections on two listeners, concurrently.
 //  Multiple connections (on the same listeners) should be processed concurrently.
 //  The received data should be echoed back to the client.
 pub async fn echoes(first: TcpListener, second: TcpListener) -> Result<(), anyhow::Error> {
-    todo!()
+    let handle1 = tokio::spawn(echo(first));
+    let handle2 = tokio::spawn(echo(second));
+    let (_outcome1, _outcome2) = tokio::join!(handle1, handle2);
+    // outcome1??;
+    // outcome2??;
+    Ok(())
+}
+
+async fn echo(listener: TcpListener) -> Result<(), anyhow::Error> {
+    loop {
+        let (mut socket, _) = listener.accept().await?;
+        tokio::spawn(async move {
+            let (mut reader, mut writer) = socket.split();
+            copy(&mut reader, &mut writer).await.unwrap();
+        });
+    }
+
 }
 
 #[cfg(test)]
